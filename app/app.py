cf373a146f2ab0047050898fbb26543cd9506acb
mablospate <pablomtcm@gmail.com>
2026-02-10 13:54:21 +0100

    fix • now auth is functional

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
+from .database import create_db
 from .routers import auth
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db()  # ← crea las tablas al arrancar
+    yield
+
+
+app = FastAPI(title="Pokemon Team Builder", lifespan=lifespan)
+
 app.include_router(auth.router)
