cd4b3828aba5d5e8924ab13effb1699d6ecb1c24
mablospate <pablomtcm@gmail.com>
2026-02-10 12:38:29 +0100

    secret management & models update

--- app/models.py
+++ app/models.py
@@ -1,21 +1,106 @@
+from typing import List
 from sqlmodel import SQLModel, Field, Relationship
 
 
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+############################################################
+#                        USER MODELS                       #
+############################################################
+
+
+class UserBase(SQLModel):
     username: str = Field(unique=True)
-    hashed_password: str
-    teams: list["Team"] = Relationship(back_populates="user")
 
 
-class Team(SQLModel, table=True):
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(SQLModel):
+    username: str | None = None
+    password: str | None = None
+
+
+class UserPublic(UserBase):
+    id: int
+
+
+############################################################
+#                     USER IN DATABASE                    #
+############################################################
+
+
+class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
+    teams: List["Team"] = Relationship(back_populates="user")
+
+
+############################################################
+#                        TEAM MODELS                       #
+############################################################
+
+
+class TeamBase(SQLModel):
     name: str
+
+
+class TeamCreate(TeamBase):
+    pass
+
+
+class TeamUpdate(SQLModel):
+    name: str | None = None
+
+
+class TeamPublic(TeamBase):
+    id: int
+    user_id: int
+
+
+class TeamPublicWithPokemon(TeamPublic):
+    pokemon: List["PokemonPublic"] = []
+
+
+############################################################
+#                     TEAM IN DATABASE                    #
+############################################################
+
+
+class Team(TeamBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     user: User = Relationship(back_populates="teams")
+    pokemon: list["Pokemon"] = Relationship(back_populates="team")
 
 
-class Pokemon(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+############################################################
+#                      POKEMON MODELS                     #
+############################################################
+
+
+class PokemonBase(SQLModel):
     name: str
-    team_id: int = Field(foreign_key="team.id")
+
+
+class PokemonCreate(PokemonBase):
+    pass
+
+
+class PokemonUpdate(SQLModel):
+    name: str | None = None
+
+
+class PokemonPublic(PokemonBase):
+    id: int
+    team_id: int
+
+
+############################################################
+#                    POKEMON IN DATABASE                   #
+############################################################
+
+
+class Pokemon(PokemonBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    tesm_id: int = Field(foreign_key="team.id")
+    team: "Team" = Relationship(back_populates="pokemon")
