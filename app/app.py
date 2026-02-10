bfcb58b569c37013e0dc7606a5abdf2b2606e451
mablospate <pablomtcm@gmail.com>
2026-02-10 18:34:40 +0100

    front added

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .database import create_db
 from .routers import auth, teams
 
@@ -11,6 +14,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Pokemon Team Builder", lifespan=lifespan)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(teams.router)
